0fe4f9302d932c4e83060dd81debdd8babb223a2
hamza <hamzakhalem97@gmail.com>
2023-10-28 12:24:21 +0100

    first init

---
+++ books/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from graphene_django.views import GraphQLView
+from .schema import schema
+
+urlpatterns = [
+    path('garphql', GraphQLView.as_view(graphiql=True, schema=schema))
+]
